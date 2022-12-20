ca03c93e7fb8709256d1e05c46f33d9867f84ba3
Shahin-Alz <shaalemzadeh@gmail.com>
2022-12-20 09:00:14 +0100

    ajout

---
+++ public/js/main.js
@@ -0,0 +1,19 @@
+let phrase = `Bonjour tout le monde `;
+
+console.log(phrase.length);
+
+console.log(phrase.trim());
+
+console.log(phrase.length);
+
+// phrase.substring(phrase.length-1));
+
+// console.log(delete phrase.indexOf(`Bonjour`))
+
+console.log(phrase.slice(7, 21));
+
+console.log(phrase.replace('Bonjour', 'string'));
+
+console.log(phrase.toUpperCase(1));
+
+
